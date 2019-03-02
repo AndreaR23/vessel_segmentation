f61b43e736187ee7aa88d938b42514371cf6c27e
AndreaR23 <ajka.rozhonova@centrum.cz>
2019-03-02 21:30:10 +0100

    Split class for augmentation

--- unet_project/data_augmentation.py
+++ unet_project/data_augmentation.py
@@ -1,13 +1,7 @@
 import numpy as np
-import cv2
-from scipy.misc import imrotate, imresize
 from skimage.transform import resize
-import os
 from tqdm import tqdm
-from keras.preprocessing import image
 import imutils
-from PIL import Image
-from sklearn.feature_extraction.image import extract_patches_2d
 
 
 class DataAugmentation:
@@ -20,19 +14,19 @@ class DataAugmentation:
         self._output_images = []
         self._output_masks = []
 
-    def random_horizontal_flip(self, img, mask, p=0.5):
+    def _random_horizontal_flip(self, img, mask, p=0.5):
         if p < np.random.rand():
             return img[:, ::-1, :], mask[:, ::-1]
         else:
             return img, mask
 
-    def random_vertical_flip(self, img, mask, p=0.5):
+    def _random_vertical_flip(self, img, mask, p=0.5):
         if p < np.random.rand():
             return img[::-1, :, :], mask[::-1, :]
         else:
             return img, mask
 
-    def random_squared_crop(self, img, mask, minimal_relative_crop_size=0.5):
+    def _random_squared_crop(self, img, mask, minimal_relative_crop_size=0.5):
         h, w, _ = mask.shape
 
         size = np.random.randint(int(minimal_relative_crop_size * h), h)
@@ -41,46 +35,19 @@ class DataAugmentation:
         return img[crop_x_origin: crop_x_origin + size, crop_y_origin: crop_y_origin + size, :], \
                mask[crop_x_origin: crop_x_origin + size, crop_y_origin: crop_y_origin + size]
 
-    def random_rotate(self, img, mask, p=0.5):
+    def _random_rotate(self, img, mask, p=0.5):
         if np.random.rand() > p:
             return img, mask
         else:
             angle = np.random.randint(0, 180)
             return imutils.rotate_bound(img, angle), imutils.rotate_bound(mask, angle)
 
-    def resize(self, img, mask):
+    def _resize(self, img, mask):
         _size = (np.random.randint(4, 2 * img.shape[0]), np.random.randint(4, 2 * img.shape[1]))
         return resize(img, _size, mode='constant', preserve_range=True), \
                resize(mask, _size, mode='constant', preserve_range=True)
 
-    def create_patches(self, img, mask):
-        h, w, _ = img.shape
-
-        if h == self._img_height and w == self._img_width:
-            return img, mask
-
-        elif h < self._img_height and w < self._img_width:
-            help_matrix = np.array([[[0 for x in range(3)] for y in range(self._img_height)] \
-                                    for z in range(self._img_width)])
-            help_matrix[0:h, 0:w, :] = help_matrix[0:h, 0:w, :] + img[:, :, :]
-
-            help_matrix_mask = np.array([[0 for x in range(self._img_height)] for y in range(self._img_width)])
-            help_matrix_mask[0:h, 0:w] = help_matrix_mask[0:h, 0:w] + mask[:, :, 0]
-            return help_matrix, help_matrix_mask
-
-        elif h < self._img_height and w >= self._img_width:
-            # Do cerne matice a patches
-            pass
-
-        elif h >= self._img_height and w < self._img_width:
-            # Do cerne matice a patches
-            pass
-
-        elif h > self._img_height and w > self._img_width:
-            # Patches
-            pass
-
-    def extend_database(self):
+    def _extend_database(self):
         print('\nExtending database.')
 
         for key, val in tqdm(self._input_images_masks.items()):
@@ -88,20 +55,19 @@ class DataAugmentation:
             mask = val[1]
 
             self._output_images.append(img.astype(np.uint8))
-            self._output_masks.append(mask)
+            self._output_masks.append(np.squeeze(mask))
 
             for i in range(0, self._how_many):
-                horizontal_flipped_img, horizontal_flipped_mask = self.random_horizontal_flip(img, mask)
-                vertical_flipped_img, vertical_flipped_mask = self.random_vertical_flip(horizontal_flipped_img,
+                horizontal_flipped_img, horizontal_flipped_mask = self._random_horizontal_flip(img, mask)
+                vertical_flipped_img, vertical_flipped_mask = self._random_vertical_flip(horizontal_flipped_img,
                                                                                         horizontal_flipped_mask)
-                cropped_img, cropped_mask = self.random_squared_crop(vertical_flipped_img,
+                cropped_img, cropped_mask = self._random_squared_crop(vertical_flipped_img,
                                                                      vertical_flipped_mask)
-                rotated_img, rotated_mask = self.random_rotate(cropped_img, cropped_mask)
-                resized_img, resized_mask = self.resize(rotated_img, rotated_mask)
+                rotated_img, rotated_mask = self._random_rotate(cropped_img, cropped_mask)
+                resized_img, resized_mask = self._resize(rotated_img, rotated_mask)
 
-                self._output_images.append(resized_img.astype(np.uint8))
+                self._output_images.append(resized_img)
                 self._output_masks.append(resized_mask)
-
         return self._output_images, self._output_masks
 
 
