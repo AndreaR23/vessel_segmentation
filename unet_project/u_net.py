f20efacb842851821937f1fd077372cf324ac411
AndreaR23 <ajka.rozhonova@centrum.cz>
2019-03-03 09:37:04 +0100

    Cleaning Unet class

--- unet_project/u_net.py
+++ unet_project/u_net.py
@@ -13,23 +13,6 @@ class Unet:
         self._img_width = img_width
         self._img_channels = img_channels
 
-    def get_crop_shape(self, target, refer):
-        # width, the 3rd dimension
-        cw = (target.get_shape()[2] - refer.get_shape()[2]).value
-        assert (cw >= 0)
-        if cw % 2 != 0:
-            cw1, cw2 = int(cw / 2), int(cw / 2) + 1
-        else:
-            cw1, cw2 = int(cw / 2), int(cw / 2)
-        ch = (target.get_shape()[1] - refer.get_shape()[1]).value
-        assert (ch >= 0)
-        if ch % 2 != 0:
-            ch1, ch2 = int(ch / 2), int(ch / 2) + 1
-        else:
-            ch1, ch2 = int(ch / 2), int(ch / 2)
-
-        return (ch1, ch2), (cw1, cw2)
-
     def create_model(self):
         print('Creating model.')
         inputs = Input((self._img_height, self._img_width, self._img_channels))
